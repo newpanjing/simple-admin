d5c8b1b7f5384f47479dfd85bc4ddaa12d894197
武松 <599194993@qq.com>
2024-04-11 11:15:34 +0800

    基本布局

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    readonly VITE_HOST: string
+    readonly VITE_PORT: number
+    // 更多环境变量...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
